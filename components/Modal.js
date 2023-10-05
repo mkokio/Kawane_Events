e901b6a60661b732ec096c08b11575b0295bd737
Marc Cocchio <marccocchio@gmail.com>
2023-10-06 08:52:59 +0900

    Landing page update

---
+++ components/Modal.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import React, { useState } from 'react';
+
+function Modal(props) {
+    return (
+        <div className="modal">
+            {/* Modal content goes here */}
+        </div>
+    );
+}
+
+export default Modal;
