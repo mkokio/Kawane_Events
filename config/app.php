c697ee734a065329c8fe85eaadd556a39f796377
Marc Cocchio <marccocchio@gmail.com>
2023-11-04 23:39:32 +0900

    Removed vapor

--- config/app.php
+++ config/app.php
@@ -181,7 +181,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\VaporUiServiceProvider::class,
     ])->toArray(),
 
     /*
