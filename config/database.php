1f5ddfbd4cdb161c4c1896666206a20233a3a939
Marc Cocchio <marccocchio@gmail.com>
2023-11-07 14:57:29 +0900

    Changed defaults from MySQL to sqlite

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'sqlite'), //changed sqlite from mySQL Nov. 7
 
     /*
     |--------------------------------------------------------------------------
