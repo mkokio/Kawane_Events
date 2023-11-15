51df1aa803216e9b648ef71a30c4ffc3058519c8
Marc Cocchio <marccocchio@gmail.com>
2023-11-15 22:13:45 +0900

    Removed tailwind styles from auth files

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,32 +1,32 @@
 <x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
+    <div>
         {{ __('Check your email inbox! Click the link we sent you to verify your mail. If you didn\'t get it, we can resend it. Thanks for signing up!') }}
         (Check your email.)
     </div>
 
     @if (session('status') == 'verification-link-sent')
-        <div class="mb-4 font-medium text-sm text-green-600">
+        <div>
             {{ __('A new verification link has been sent to the email address you provided during registration.') }}
         </div>
     @endif
 
-    <div class="mt-4 flex items-center justify-between">
-        <form method="POST" action="{{ route('verification.send') }}">
+    <div class="d-grid gap-2 d-md-flex justify-content-md-end"> 
+        <form method="POST" action="{{ route('verification.send') }}" class="d-inline-block">
             @csrf
 
-            <div>
+            
                 <x-primary-button>
                     {{ __('Resend Verification Email') }}
                 </x-primary-button>
-            </div>
+            
         </form>
 
-        <form method="POST" action="{{ route('logout') }}">
-            @csrf
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
 
-            <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                {{ __('Log Out') }}
-            </button>
-        </form>
-    </div>
+                <button type="submit" class="btn btn-outline-secondary" class="d-inline-block">
+                    {{ __('Log Out') }}
+                </button>
+            </form>
+        </div>
 </x-guest-layout>
