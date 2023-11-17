ae6166a81bc94677eebe8e978f8bfa5f984d9238
Marc Cocchio <marccocchio@gmail.com>
2023-11-17 10:25:24 +0900

    Completed the modal for donate

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
+        
         <!-- Styles -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
@@ -67,9 +67,24 @@
                 </div>            
             </div>    
             <hr />
-            <a href="https://www.marccocchio.com" class="link-info">
-                &copy; マーク
-            </a><br />
-            <a href="{{ asset('kawaneeventsbitcoinqr.jpg') }}" class="link-info">{{ __('Donate') }}</a>
+            <a href="https://www.marccocchio.com" target="_blank" class="btn btn-link">&copy;2023 マーク</a>
+            <br>
+            <button type="button" class="btn btn btn-link" data-bs-toggle="modal" data-bs-target="#imageModal">
+                {{ __('Donate') }}
+            </button>
+            
+            <!-- Modal with just an image -->
+            <div class="modal fade" id="imageModal" tabindex="-1" aria-labelledby="imageModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-body text-center">
+                            <img src="{{ asset('kawaneeventsbitcoinqr.jpg') }}" alt="Bitcoin QR Code" style="width: 80%;">
+                        </div>
+                    </div>
+                </div>
+            </div>
     </body>
 </html>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
