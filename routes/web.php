57659faa920fb373719c95df520beb5c09fbaf9d
Marc Cocchio <marccocchio@gmail.com>
2023-11-22 11:42:47 +0900

    Changed verification route names

--- routes/web.php
+++ routes/web.php
@@ -50,12 +50,12 @@ Route::middleware('auth')->group(function () {
     // Email verification
     Route::get('/email/verify', function () {
         return view('auth.verify-email');
-    })->middleware('auth')->name('verification.notice');
+    })->middleware('auth')->name('verification.notice1');
 
     Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
         $request->fulfill();
         return redirect('/home');
-    })->middleware(['auth', 'signed'])->name('verification.verify');
+    })->middleware(['auth', 'signed'])->name('verification.verify1');
 
     Route::post('/email/verification-notification', function (Request $request) {
         $request->user()->sendEmailVerificationNotification();
