7a91d6e2ca06f9731d0163cb045c1109f785343e
Marc Cocchio <marccocchio@gmail.com>
2023-11-27 15:06:03 +0900

    Minor formatting

--- resources/views/components/input-time.blade.php
+++ resources/views/components/input-time.blade.php
@@ -2,7 +2,7 @@
     <div class="col-sm-6">
         <div class="d-flex align-items-center">            
             <label for="{{ $for }}" class="form-label mt-4" style="width: 150px;">{{ __($label) }}</label>
-            <input type="time" id="{{ $for }}" name="{{ $for }}" class="form-control  mt-4" {{ $attributes }}>
+            <input type="time" id="{{ $for }}" name="{{ $for }}" class="form-control mt-4" {{ $attributes }}>
         </div>  
     </div>
 </div>
