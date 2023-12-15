2e835d7abbffa9fe4df7440e5ea1ec262e9328a1
Marc Cocchio <marccocchio@gmail.com>
2023-12-15 11:31:37 +0900

    Resized images and minor refactors on Success page

--- resources/views/success.blade.php
+++ resources/views/success.blade.php
@@ -15,82 +15,33 @@
         <p>
             {{ __('Congratulations! You created an event on the ') }} <a href="{{ env('GOOGLE_CALENDAR_PUBLIC_URL') }}" target="_blank" class="custom-link" target="_blank">{{ __('Kawane Events Public Google Calendar') }}</a>.
         </p>
-        <br />
-        <div class="d-flex justify-content-center">
+        <hr>
+        <div class="text-center">
             <a href="{{ env('GOOGLE_CALENDAR_PUBLIC_URL') }}" target="_blank">
-                <img class="mx-auto w-32" src="{{ secure_asset('KWElogoSmall.png') }}" alt="Kawane Events Calendar QR Code">
+                <img width="50%" class="mx-auto" src="{{ asset('KWElogoSmall.png') }}" alt="Kawane Events Calendar QR Code">
             </a>
-        </div>
-        <div class="d-flex justify-content-center">
-            <img size="40%" class="mx-auto" src="{{ secure_asset('KawaneEventsCalendarQR.png') }}" alt="Kawane Events Calendar QR Code">
+            <a href="{{ env('GOOGLE_CALENDAR_PUBLIC_URL') }}" target="_blank">
+                <img width="50%" class="mx-auto" src="{{ asset('KawaneEventsCalendarQR.png') }}" alt="Kawane Events Calendar QR Code">
+            </a>
         </div>
 
         <br />
         <hr />
-        <br />
         <p>
             {{ __('Would you like to ') }} <a href='/dashboard' class="custom-link">{{ __('create another event') }}</a>?
         </p>
-        <br />
         <hr />
         <br />
         <x-embed-calendar />
         <hr />
-        <br />
+        <!--<br />
         <p>
         {{ __('Assistance: ') }}
         <a href="mailto:{{ env('MAIL_USERNAME') }}?subject=Kawane%20Event%20Help" class="custom-link">
         {{ env('MAIL_USERNAME') }}
         </a>
         </p>
-        <hr />
+        <hr />-->
     </div>
 </div>
-<!-- Success message and loading animation initially hidden -->
-<div class="loading" style="display: none;">
-    <img src="{{ secure_asset('logo80.png') }}" alt="Loading..." class="spin-image" />
-</div>
-
-<div class="loaded content" style="display: none;">
-    <p class="text-sm text-gray-600">{{ __('Event created successfully.') }}</p>
-</div>
-
-<!-- CSS -->
-<style>
-    /* Spinner animation styles */
-    @keyframes spin {
-        0% { transform: rotate(0deg); }
-        100% { transform: rotate(360deg); }
-    }
-
-    .spin-image {
-        animation: spin 1s infinite linear;
-    }
-</style>
-
-<!-- JavaScript -->
-<script>
-    document.addEventListener("DOMContentLoaded", function () {
-        const form = document.getElementById('event-form');
-
-        if (form) {
-            form.addEventListener('submit', function (event) {
-                event.preventDefault();
-                document.querySelector('.loading').style.display = 'block';
-                document.querySelector('.loaded').style.display = 'none';
-
-                // Simulate form submission
-                setTimeout(function () {
-                    // Assuming successful submission after a delay
-                    document.querySelector('.loading').style.display = 'none';
-                    document.querySelector('.loaded').style.display = 'block';
-                    
-                    // Redirect to eventcreatesuccess route after successful submission
-                    window.location.href = "{{ route('eventcreatesuccess') }}"; // Replace with your actual route
-                }, 2000); // Simulated 2-second delay, replace with your logic
-            });
-        }
-    });
-</script>
-
 </x-app-layout>
