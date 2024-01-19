76fdd510ec6120691ad2adfdb60a2fb37ad95423
Marc Cocchio <marccocchio@gmail.com>
2024-01-19 15:21:30 +0900

    Updated MVC to allow users to 'delete' their events

--- resources/views/myevents.blade.php
+++ resources/views/myevents.blade.php
@@ -14,11 +14,9 @@
                 <h3>{{  __('My Events') }}</h3>
                 <p class="lead">{{ __('Here is a list of events which you have created.') }}</p>
             
-                <!-- logic goes below -->
-
                 <div class="accordion" id="accordionOfEvents">
                     @foreach ($events as $index => $event)
-                        @if ($event->google_calendar_id)
+                        @if ($event->google_calendar_id)<!--Display all of your events which have a google_calendar_id -->
                             <div class="accordion-item">
                                 <h2 class="accordion-header">
                                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse{{ $index }}" aria-expanded="false" aria-controls="collapse{{ $index }}">
@@ -29,7 +27,28 @@
                                     <div class="accordion-body">
                                         {{ $event->description ?? 'N/A' }}
                                         <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                                            <button type="button" class="btn btn-outline-danger btn-sm">{{  __('Delete Event') }}</button>
+                                            <button type="button" class="btn btn-outline-danger btn-sm" data-bs-toggle="modal" data-bs-target="#staticBackdrop{{ $event->id }}">{{ __('Delete') }}</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                
+                            <div class="modal fade" id="staticBackdrop{{ $event->id }}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h1 class="modal-title fs-5" id="staticBackdropLabel">{{ __('Are you sure?') }}</h1>
+                                        </div>
+                                        <div class="modal-body">
+                                            {{ __('This will remove your event from the Google Calendar. This cannot be undone.') }}
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">{{ __('Cancel') }}</button>
+                                            <form action="{{ route('eventforms.destroy', $event->id) }}" method="post">
+                                                @csrf
+                                                @method('delete')
+                                                <button type="submit" class="btn btn-danger">{{ __('Delete Event') }}</button>
+                                            </form>
                                         </div>
                                     </div>
                                 </div>
@@ -38,11 +57,52 @@
                     @endforeach
                 </div>
                 
-
+        
+                
+                  
+<!--
+        <div id="loading" class="loading">
+            <div class="spinner">
+                <img src="{{ secure_asset('logo80.png') }}" alt="Saving Profile..." class="spin-image" />
+            </div>
+        </div>
+-->
     </section>
     
     <style>
+    .confirmationModal {
+        display: none;
+    }
 
+    /* Deleted animation */
+    @keyframes spin {
+    0%, 100% {
+        transform: scale(0);
+    }
+    50% {
+        transform: scale(1.2);
+    }
+    }
+
+    /* Apply the animation to the image */
+    .spin-image {
+        animation: spin 1s infinite linear;
+    }
+
+    /* Center the spinner within the content area */
+    .py-12 {
+        position: relative;
+        min-height: 100vh; /* Ensure the content area fills the viewport */
+    }
+
+    .loading {
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        display: none; /* Hide initially */
+    }
+    
     </style>
 
     <script>
